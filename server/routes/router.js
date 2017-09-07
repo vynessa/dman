e7836b337758f5856b6d725f200af0dc3ba6787c
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-09-07 23:05:47 +0100

    Modify files

--- server/routes/router.js
+++ server/routes/router.js
@@ -8,7 +8,7 @@ router.get('/api/v1', (req, res) => res.status(200).send({
   message: 'Welcome to the dMan API!'
 }));
 
-// Users endpoint
+/** Users endpoint */
 router.get('/api/v1/users', UsersController.getUsers)
   .get('/api/v1/users/:id', UsersController.findUser)
   .post('/api/v1/users/createuser', UsersController.createUser)
@@ -19,7 +19,7 @@ router.get('/api/v1/users', UsersController.getUsers)
   .get('/api/v1/users/:id/documents', UsersController.findUserDocuments)
   .get('/api/v1/search/users', UsersController.searchUsers);
 
-// Documents endpoint
+/** Documents endpoint */
 router.get('/api/v1/documents', DocsController.getDocuments)
   .post('/api/v1/documents', DocsController.createDocument)
   .put('/api/v1/documents/:id', DocsController.updateDocument)
